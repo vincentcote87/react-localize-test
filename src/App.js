c8f06b4363a549224d60f9ff8f9eb7f3c0061c44
Vincent Cote <vincent_cote@transcanada.com>
2019-11-05 10:28:39 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import i18next from 'i18next';
 
-function App() {
+i18next.init({
+    lng: 'en',
+    debug: 'true',
+    resources: {
+        en: {
+            translation: {
+                'fname': 'First name',
+                'lname': 'Last name',
+                'submitBtn': 'Continue',
+                'langBtn': 'Fr'
+            }
+        },
+        fr: {
+            translation: {
+                'fname': 'Premier nom',
+                'lname': 'Nom de famille',
+                'submitBtn': 'Continuer',
+                'langBtn': 'En'
+            }
+        }
+    }
+}, () => {});
+
+export default function App() {
+    const [lang, setLang] = useState(i18next.language);
+    function changeLang(lang) {
+        i18next.changeLanguage(lang);
+        setLang(lang);
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <button onClick={() => {changeLang(lang === 'fr' ? 'en' : 'fr')}}>{lang.toUpperCase()}</button>
+          <form >
+              <label htmlFor="fname">{i18next.t('fname')}</label><br/>
+              <input type="text" name={"fname"}/><br/>
+              <label htmlFor="lname">{i18next.t('lname')}</label><br/>
+              <input type="text" name={"lname"}/>
+          </form>
+          <button>{i18next.t('submitBtn')}</button>
+      </div>
+  )
 }
-
-export default App;
